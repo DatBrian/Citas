a2d0c58ac2f2abdb512deaeb40f4deb28953e247
DatBrian <vikard.2005@hotmail.com>
2023-08-18 07:36:42 -0500

    build: :pushpin: Se configuró dotenv para el index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT || 3000;
-const host = process.env.HOST || 'localhost';
+const host = process.env.HOST || "localhost";
 
 app.listen(port, () => {
-    console.log(`listening on http://${host}:${port}`);
-})
+  console.log(`listening on http://${host}:${port}`);
+});
